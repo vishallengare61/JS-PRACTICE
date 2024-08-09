68293a084472b296330e7050942c675d155d6ea6
Vishal <124039667+vishallengare61@users.noreply.github.com>
2024-08-09 10:38:35 +0000

    Trying to learn the confusion conversions in js

---
+++ comparison.js
@@ -0,0 +1,8 @@
+console.log("2" > 1);
+console.log("02" > 1);  // as discussed last Covnersional-operator.js file, if u take the string with any word then it will give is true 
+
+
+console.log( null == 0);
+console.log( null < 0);
+console.log( null <= 0);
+console.log( null >= undefined);

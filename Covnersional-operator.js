734b9762d7daedbee13922d8ad2fa31c0aa61120
Vishal <124039667+vishallengare61@users.noreply.github.com>
2024-08-09 09:43:32 +0000

    Learn the conversion of datatypes

---
+++ Covnersional-operator.js
@@ -0,0 +1,43 @@
+// let score = 33;
+
+// console.log(typeof(score));    //checking the data type of given numbe.
+
+// let score2 = "33";
+// console.log(typeof(score2)); 
+
+// let numCovert = Number(score2)  //Number method will covert the string into the number
+
+
+// console.log(typeof(numCovert)); 
+
+
+// let str = "Vishal";
+// let cstr = Number(str);
+// console.log(cstr);    //these will print the output as NaN (not a number) and the data type of NaN is number
+// console.log(typeof(cstr));   // the data type of NaN is number so output will be number
+
+
+// let num = null;
+
+// console.log(num);         
+// console.log(typeof num);   //the data type of null is object
+
+
+// let num = undefined;
+
+// let num2 = Number(num)
+// console.log(num2); 
+// console.log(num2); 
+
+// console.log(typeof num); 
+
+
+// let booVal = 1
+// let val = Boolean(booVal);
+// console.log(typeof val)
+
+//for the boolen value the IMP notes,
+
+// 1 => true,  0  => false
+//  ""  => false     [if we pass the empty string then it will give me false]
+// "vishal" => true  [if we pass any string value then it will give me true]  

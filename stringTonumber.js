299777164bcfc302dfc1ff2875acf36dc7a87f25
Vishal <124039667+vishallengare61@users.noreply.github.com>
2024-08-09 10:09:19 +0000

    Learn about the string operations with the numbers.

---
+++ stringTonumber.js
@@ -0,0 +1,37 @@
+// let val = 3;
+// let negval = -val;
+// console.log(negval);  // will convert the number into negative.
+
+// let str1 = "hello";
+// let str2 = " Vishal"
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("hello" + " Vishal");
+
+// console.log("1" + "1");      //these will print the 11 cause two strings will get concanitted.
+
+// console.log(2 + "3");      // these will print the 23 cause here if one is string and one is number then it will cancanate them rather than the addition.
+
+// console.log("1" + 2 + 2);   // output wi;; 122  if the string is first then it will concat them all.
+
+// console.log(1 + 2 + "2");   //output will be 32 if the string is last and the first two or more are numbers then it will add them first and then concate with the string.
+
+// console.log( 1 + 2 + 3 + 4 + "3" + 2 + 4) // o/p => 10324
+
+
+// console.log( 1 + 2 + 3 + 4 + "3" + 2 + 4 + 5) // o/p => 103245    after the string all numbers will concate.
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;   //here in all num1, num2, mun3 will store value as 4;
+
+console.log(num1);  //4
+console.log(num2);  //4
+console.log(num3);  //4
+
+
+

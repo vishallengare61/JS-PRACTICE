2c64d1517b2d208cdb52fcb57cdf4fd9cc243c76
Vishal <124039667+vishallengare61@users.noreply.github.com>
2024-08-09 11:11:05 +0000

    Understand the types of data types in js.

---
+++ dataTypes.js
@@ -0,0 +1,33 @@
+// there is two types of data types - primitive and non-primitive(referance)
+
+// Primitive - 
+// 7 types --
+// 1)String, 2)Number, 3)boolean, 4)null 5)undefined  6)symbole 7)BigInt
+
+// non-primitive(referance)  
+
+// Array objects Functions 
+
+
+
+
+
+
+
+
+// const id = Symbol('123')
+// const anotheerId = Symbol('123')
+
+// console.log(id === anotheerId)
+// console.log(id)
+// console.log(anotheerId)
+
+// Is javascript is a static type of dynamically type language?
+//==> javascript is the dynamic typed language these means that the variables types are determined at the runtime 
+      //and you do not need to explicitly declare the type of varaible before using it.
+
+
+
+    //   let num = 1232332338748326487264782647826487236n
+        //  let num = 908989n
+    // when you put the small 'n' at the end of the number then it will become the bigInt data type

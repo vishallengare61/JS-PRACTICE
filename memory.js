1eadabb1de2963d0ec221a42891fa2fa7c5078aa
Vishal <124039667+vishallengare61@users.noreply.github.com>
2024-08-09 11:26:13 +0000

    tried to learn the basic memory managment in js.

---
+++ memory.js
@@ -0,0 +1,7 @@
+// stack and heap memory --
+
+// there is two types data type , primitive and non-primitive
+
+// so all primitive data types are store in stack memory and all non-primitive data types are present in the heap memore.
+
+// all variables stored in stack memory.
